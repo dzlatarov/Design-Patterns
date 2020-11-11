10c2350963e0f09e844b2ff20024f4d4ccc2bc58
Danail Zlatarov <38438268+dzlatarov@users.noreply.github.com>
2020-11-11 13:54:11 +0200

    Add files via upload

---
+++ Creational-Patterns/BuilderPattern/Director.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern
+{
+    public class Director
+    {
+        private IBuilder builder;
+
+        public void SetBuilder(IBuilder builder)
+        {
+            this.builder = builder;
+        }
+
+        public void ConstructorStoneHouse(IBuilder builder)
+        {
+            builder.Reset();
+            builder.SetDoors(2);
+            builder.SetGarden(true);
+            builder.SetType("Stone");
+            builder.SetWindows(4);            
+        }
+
+        public void ConstructorWoodenHouse(IBuilder builder)
+        {
+            builder.Reset();
+            builder.SetDoors(1);
+            builder.SetGarden(false);
+            builder.SetType("Wood");
+            builder.SetWindows(4);
+        }
+    }
+}
---
+++ Creational-Patterns/BuilderPattern/WoodenHouse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern
+{
+    public class WoodenHouse : IHouse
+    {
+        public WoodenHouse()
+        {
+
+        }
+
+        public int Door { get; set; }
+
+        public int Window { get; set; }
+
+        public bool IsGarden { get; set; }
+
+        public string Type { get; set; }
+    }
+}
